9a8a267bab84e82a738e6d7bf6cc6e77194c5ea2
Isak <isak.lefevre@hotmail.se>
2025-06-13 22:01:31 +0200

    feat: implement basic qtable decisionmaking

--- frozenlake/main.py
+++ frozenlake/main.py
@@ -1,13 +1,28 @@
 import gymnasium as gym
+import numpy as np
 
-env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="human")
+env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="human", max_episode_steps=100)
 observation, info = env.reset()
+
+num_states = env.observation_space.n
+num_actions = env.action_space.n
+qtable = np.zeros((num_states, num_actions))
+
 
 episode_over = False
 while not episode_over:
-    action = env.action_space.sample()
+    max_reward_action = np.argmax(qtable[observation])
+    if (qtable[observation, max_reward_action] == 0):
+        print("No value in qtable, I pick random!!")
+        action = env.action_space.sample()
+    else:
+        print("I know exactly what to do!")
+        action = max_reward_action
+
     observation, reward, terminated, truncated, info = env.step(action)
 
     episode_over = terminated or truncated
 
 env.close()
+if reward > 0:
+    print("HE DIDIT BOYS")
